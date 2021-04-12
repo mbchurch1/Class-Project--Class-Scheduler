d3b6ed1cf5e48f43750dfb88924de4fbc33a27d6
wsgoodwi <wsgoodwi@ncsu.edu>
2021-04-12 17:35:40 -0400

    [Implementation] Updated Stack without 'public' designation and with complete javadoc

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/Stack.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/Stack.java
@@ -11,14 +11,34 @@ package edu.ncsu.csc216.pack_scheduler.util;
  */
 public interface Stack<E> {
 	
-	public void push(E element);
+	/**
+	 * Adds the element to the top of the Stack
+	 * @param element the element being added to top of Stack
+	 */
+	void push(E element);
 	
-	public E pop();
+	/**
+	 * Removes the element at top of Stack
+	 * @return the element that was removed from top of Stack
+	 */
+	E pop();
 	
-	public boolean isEmpty();
+	/**
+	 * Determines if the Stack is empty
+	 * @return true if the Stack is empty
+	 */
+	boolean isEmpty();
 	
-	public int size();
+	/**
+	 * Determines the number of elements in the Stack
+	 * @return the number of elements in the Stack
+	 */
+	int size();
 	
-	public void setCapacity(int capacity);
+	/**
+	 * Sets the Stack's capacity
+	 * @param capacity the maximum number of elements that can be added to Stack
+	 */
+	void setCapacity(int capacity);
 
 }
